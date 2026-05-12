47b98eb3fc3f66c4c708b2c7a3927dcc0a8f36f7
Biuuu <andinipratiwi235@gmail.com>
2026-05-12 14:10:31 +0700

    feat

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -3,7 +3,14 @@ import * as controller from '../controllers/task.controller'
 import { validate } from '../middlewares/validate.middleware'
 import { createTaskSchema, updateTaskStatusSchema } from '../schemas/task.schema'
 
-// / POST
-// / GET
-// /:id/status PATCH
-// /:id DELETE
+const router = Router()
+
+router.post('/', validate(createTaskSchema), controller.createTask)
+
+router.get('/', controller.getTasks)
+
+router.patch('/:id/status', validate(updateTaskStatusSchema), controller.updateTaskStatus)
+
+router.delete('/:id', controller.deleteTask)
+
+export default router
