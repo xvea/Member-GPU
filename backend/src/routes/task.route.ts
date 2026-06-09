bc1f54b8731b5b7bfd121d82b1b6890377a26ff9
Surya Pradipta <111216828+xvea@users.noreply.github.com>
2026-06-09 09:52:08 +0700

    feat(backend): Update task route to use new updateTaskSchema

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import * as controller from '../controllers/task.controller'
 import { validate } from '../middlewares/validate.middleware'
-import { createTaskSchema, updateTaskStatusSchema } from '../schemas/task.schema'
+import { createTaskSchema, updateTaskSchema } from '../schemas/task.schema'
 
 const router = Router()
 
@@ -9,7 +9,7 @@ router.post('/', validate(createTaskSchema), controller.createTask)
 
 router.get('/', controller.getTasks)
 
-router.patch('/:id/status', validate(updateTaskStatusSchema), controller.updateTaskStatus)
+router.patch('/:id', validate(updateTaskSchema), controller.updateTask)
 
 router.delete('/:id', controller.deleteTask)
 
